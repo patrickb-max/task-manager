c1efbe4e80c617b093de5ed2a322c82f3a24bad0
patrickb-max <patrick.b@futuristglobal.com>
2026-07-22 10:09:37 +0000

    connecting or route backend to frontend

--- task-manager-frontend/src/routes/+page.svelte
+++ task-manager-frontend/src/routes/+page.svelte
@@ -1,9 +1,37 @@
-// Individual Frontend code in src routes .svelte 
-// Display whats there in this file .
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script>
-    let message = "Hello Svelte!";
+    let tasks = [];
+
+    // Load tasks from backend
+    async function loadTasks() {
+        const res = await fetch("http://localhost:5000/tasks");
+        tasks = await res.json();
+    }
+
+    loadTasks();
 </script>
 
-<h1> Welcome to Patrick</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Individual Frontend code in src routes .svelte 
+// Display whats there in this file .
+//<h1>Welcome to SvelteKit</h1>
+//<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+//<script>
+ //   let message = "Hello Svelte!";
+//</script>
+
+//<h1> Welcome to Patrick</h1>
