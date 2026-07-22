a905ad99bad2919e8573a99942b52a0b229ed694
patrickb-max <patrick.b@futuristglobal.com>
2026-07-22 09:47:40 +0000

    indivdual frontend file using src-routes-pagepage.svelte

--- task-manager-frontend/src/routes/+page.svelte
+++ task-manager-frontend/src/routes/+page.svelte
@@ -1,3 +1,5 @@
+// Indivdual Frontend code in src routes .svelte 
+// Display whats there in this file .
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script>
