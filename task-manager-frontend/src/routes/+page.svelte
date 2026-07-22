d5b02d22d347903527b5493b7d57278541e50538
patrickb-max <patrick.b@futuristglobal.com>
2026-07-22 10:13:00 +0000

    removing dual script block

--- task-manager-frontend/src/routes/+page.svelte
+++ task-manager-frontend/src/routes/+page.svelte
@@ -30,8 +30,5 @@
 // Display whats there in this file .
 //<h1>Welcome to SvelteKit</h1>
 //<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-//<script>
- //   let message = "Hello Svelte!";
-//</script>
 
 //<h1> Welcome to Patrick</h1>
